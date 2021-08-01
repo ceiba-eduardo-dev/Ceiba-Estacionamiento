3387819cbcc030e686aa09ebfefe297d77f5e261
ceiba-eduardo-dev <87191133+ceiba-eduardo-dev@users.noreply.github.com>
2021-08-01 13:23:32 -0500

    jacoco

--- app/src/test/java/com/ceiba/estacionamiento/vehicles/model/vo/DatePeriodTest.kt
+++ app/src/test/java/com/ceiba/estacionamiento/vehicles/model/vo/DatePeriodTest.kt
@@ -2,8 +2,10 @@ package com.ceiba.estacionamiento.vehicles.model.vo
 
 import com.ceiba.estacionamiento.vehicles.model.valueobjects.DatePeriod
 import junit.framework.TestCase
+import org.junit.Test
 
 class DatePeriodTest : TestCase() {
+    @Test
     fun testTotalHoursByDate() {
         val datePeriod = DatePeriod("2021-11-11", "2021-11-12")
         assertEquals(24, datePeriod.totalHoursByDate())
