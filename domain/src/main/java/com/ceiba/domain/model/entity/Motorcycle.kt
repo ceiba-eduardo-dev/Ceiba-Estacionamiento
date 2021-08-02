b50e42df89ada374cb8ec5642c10f362d2a1818b
ceiba-eduardo-dev <87191133+ceiba-eduardo-dev@users.noreply.github.com>
2021-08-02 01:38:02 -0500

    no admite return

--- domain/src/main/java/com/ceiba/domain/model/entity/Motorcycle.kt
+++ domain/src/main/java/com/ceiba/domain/model/entity/Motorcycle.kt
@@ -4,7 +4,5 @@ class Motorcycle(
     private val licensePlate: String,
     private val cylinderCapacity: Int
 ) : Vehicle(licensePlate) {
-    fun getCylinderCapacity(): Int {
-        return cylinderCapacity
-    }
+    fun getCylinderCapacity(): Int = cylinderCapacity
 }
